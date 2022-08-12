0266f90bd23e90a801168bd8369f8241b41b96b6
Vincent <v3rdugo@yahoo.com>
2022-08-11 19:34:22 -0700

    initial commit

---
+++ helper-hardhat-config.js
@@ -0,0 +1,31 @@
+const { ethers } = require("hardhat")
+
+const networkConfig = {
+    4: {
+        name: "rinkeby",
+        vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+        subscriptionId: 10157,
+        callbackGasLimit: 500000,
+        interval: "30",
+        mintFee: "100000000000000000", // 0.01 ETH
+        ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+    },
+    31337: {
+        name: "localhost",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        subscriptionId: 10157,
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+        callbackGasLimit: 500000,
+        interval: "30",
+        mintFee: "100000000000000000" // 0.01 ETH
+    }
+}
+
+const developmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+    networkConfig,
+    developmentChains
+}
