edf764b762bccfaa9694824cd317585c8444379c
Vincent <v3rdugo@yahoo.com>
2022-08-13 15:43:05 -0700

    add some getter functions

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -152,4 +152,14 @@ contract NftMarketplace is ReentrancyGuard
             revert NftMarketplace__TransferFailed();
         }
     }
+
+    function getListing(address nftAddress, uint256 tokenId) external view returns(Listing memory)
+    {
+        return s_listings[nftAddress][tokenId];
+    }
+
+    function getProceeds(address seller) external view returns(uint256)
+    {
+        return s_proceeds[seller];
+    }
 }
