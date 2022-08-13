840d1b5a48e8fc5cc1222fc2c0eb56ff09e738a1
Vincent <v3rdugo@yahoo.com>
2022-08-13 16:29:29 -0700

    fix naming issue

--- contracts/test/BasicNft.sol
+++ contracts/test/BasicNft.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract BasicNFT is ERC721
+contract BasicNft is ERC721
 {
     string public constant TOKEN_URI =
     "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
