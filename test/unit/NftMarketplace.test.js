878ad10e1290b12f8a2d176124fbabb833ee8497
Vincent <v3rdugo@yahoo.com>
2022-08-15 15:36:56 -0700

    buyItem tests all passing

--- test/unit/NftMarketplace.test.js
+++ test/unit/NftMarketplace.test.js
@@ -10,6 +10,8 @@ describe("NftMarketplace Unit Tests", async () =>
 
     const PRICE = ethers.utils.parseEther("0.1")
 
+    const INSUFFICIENT_PRICE = ethers.utils.parseEther("0.05")
+
     const TOKEN_ID = 0
 
     beforeEach(async () =>
@@ -92,6 +94,74 @@ describe("NftMarketplace Unit Tests", async () =>
             await basicNft.approve(user.address, TOKEN_ID)
 
             await expect(nftMarketplace.cancelListing(basicNft.address, TOKEN_ID)).to.be.reverted
+        })
+
+        it("updates the listings mapping when an item is cancelled", async () =>
+        {
+            await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)
+
+            await nftMarketplace.cancelListing(basicNft.address, TOKEN_ID)
+
+            const listing = await nftMarketplace.getListing(basicNft.address, TOKEN_ID)
+
+            assert(listing.price.toString() === '0', 'listing.price !== 0')
+            assert(listing.seller.startsWith('0x'), 'listing.seller does not start with "0x"')
+
+        })
+    })
+
+    describe("buyItem", async () =>
+    {
+        it("makes sure the NFT is listed before it can be bought", async () =>
+        {
+            await expect(nftMarketplace.buyItem(basicNft.address, TOKEN_ID, {value: PRICE})).to.be.reverted
+        })
+
+        it("reverts if the sale price of the NFT is not met", async () =>
+        {
+            await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)
+
+            await expect(nftMarketplace.buyItem(basicNft.address, TOKEN_ID, {value: INSUFFICIENT_PRICE})).to.be.reverted
+        })
+
+        it("updates the proceeds mapping such that the seller balance increases by the sale price of the NFT", async () =>
+        {
+            await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)
+            await nftMarketplace.buyItem(basicNft.address, TOKEN_ID, {value: PRICE})
+
+            const sellerProceeds = await nftMarketplace.getProceeds(deployer.address)
+
+            assert(sellerProceeds.toString() === PRICE.toString(), 'sellerProceeds.toString() !== PRICE.toString()')
+        })
+
+        it("deletes the listing for that NFT after the sale is complete", async () =>
+        {
+            await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)
+            await nftMarketplace.buyItem(basicNft.address, TOKEN_ID, {value: PRICE})
+
+            const listing = await nftMarketplace.getListing(basicNft.address, TOKEN_ID)
+
+            assert(listing.price.toString() === '0', 'listing.price !== 0')
+            assert(listing.seller.startsWith('0x'), 'listing.seller does not start with "0x"')
+        })
+
+        it("transfers ownership of the NFT to the buyer", async () =>
+        {
+            await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)
+
+            nftMarketplace = nftMarketplaceContract.connect(user)
+
+            await nftMarketplace.buyItem(basicNft.address, TOKEN_ID, {value: PRICE})
+
+            const newOwner = await basicNft.ownerOf(TOKEN_ID)
+
+            assert(newOwner.toString() === user.address, 'newOwner.toString() !== user.address')
+        })
+
+        it("emits an ItemBought event on sale of the NFT", async () =>
+        {
+            await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)
+            expect(await nftMarketplace.buyItem(basicNft.address, TOKEN_ID, {value: PRICE})).to.emit("ItemBought")
         })
     })
 
