4c5dbb862ea0a1955d91dab979d7a7db8fb29cb2
Vincent <v3rdugo@yahoo.com>
2022-08-17 17:20:33 -0700

    create a script for automatically minting an NFT then listing it on the marketplace

---
+++ scripts/mint-and-list.js
@@ -0,0 +1,32 @@
+const { ethers } = require("hardhat")
+
+const PRICE = ethers.utils.parseEther('0.1')
+
+
+const mintAndList = async () =>
+{
+    const nftMarketplace = await ethers.getContract("NftMarketplace")
+    const basicNft = await ethers.getContract("BasicNft")
+
+    console.log('Minting...')
+    const mintTx = await basicNft.mintNft()
+    const mintTxReceipt = await mintTx.wait(1)
+    const tokenId = mintTxReceipt.events[0].args.tokenId
+    
+    console.log('Approving NFT...')
+    const approvalTx = await basicNft.approve(nftMarketplace.address, tokenId)
+
+    console.log('Listing NFT...')
+    const listTx = await nftMarketplace.listItem(basicNft.address, tokenId, PRICE)
+    listTx.wait(1)
+    console.log('Listed!')
+
+}
+
+mintAndList()
+    .then(() => process.exit(0))
+    .catch((error) =>
+    {
+        console.log(error)
+        process.exit(1)
+    })
